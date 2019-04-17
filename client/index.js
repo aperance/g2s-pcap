4ed42777612a052a3783b3e057ff0a1eebcdb211
William Aperance <aperance@gmail.com>
2019-04-17 01:00:48 -0700

    Output raw message to DOM

--- client/index.js
+++ client/index.js
@@ -2,7 +2,7 @@ import React, { useState, useRef, useEffect } from "react";
 import ReactDOM from "react-dom";
 
 function App() {
-  const [status, setStatus] = useState("disconnected");
+  const [captureStream, setCaptureStream] = useState([]);
   const [retry, setRetry] = useState(false);
   const socket = useRef(null);
 
@@ -10,18 +10,20 @@ function App() {
     if (!socket.current || retry) {
       const ws = new WebSocket("ws://localhost:3000/ws");
 
-      ws.onopen = () => setStatus("connected");
-      ws.onmessage = x => {
+      ws.onopen = () => console.log("Connection established");
+      ws.onmessage = message => {
         try {
-          console.log(JSON.parse(x.data));
+          const obj = JSON.parse(message.data);
+          console.log(obj);
+          const xml = obj.payload.split("\r\n", 2)[1];
+          setCaptureStream(z => z.concat({ ...obj, xml }));
         } catch (err) {
           console.error(err);
           ws.onclose = null;
           ws.close();
-          setStatus("dataError");
         }
       };
-      ws.onerror = () => setStatus("connectionError");
+      ws.onerror = () => console.log("Connection Error");
       ws.onclose = () => setTimeout(() => setRetry(true), 5000);
 
       setRetry(false);
@@ -29,7 +31,11 @@ function App() {
     }
   }, [retry]);
 
-  return <div>{status}</div>;
+  return (
+    <div>
+      <pre>{captureStream.map(x => x.payload)}</pre>
+    </div>
+  );
 }
 
 var mountNode = document.getElementById("app");
